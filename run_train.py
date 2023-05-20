00b4dc4f1bdf9a95c0104d314adb4546816cbf8c
phdfar <iplserver01@gmail.com>
2023-05-20 12:39:07 +0430

    add pth

--- run_train.py
+++ run_train.py
@@ -1,17 +1,17 @@
 import random
-from dataloader import correc,binaryt
+from dataloader import correct,binary
 from net import simple_backbone
 
 def start(args):
     if args.task=='correct':
         dataloader_train,dataloader_val=correct.run(args)
         if args.network=='simple_backbone':
-            simple_backbone.run(args,dataloader_train,dataloa
+            simple_backbone.run(args,dataloader_train,dataloader_val)
     
     elif args.task=='binary':
         dataloader_train,dataloader_val=binary.run(args)
         if args.network=='simple_binary':
-            simple_binary.run(args,dataloader_train,dataloader_val)der_val)
+            simple_binary.run(args,dataloader_train,dataloader_val)
         
         
 
