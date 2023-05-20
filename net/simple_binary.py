232d59db5246e7bd49bd2c9cd246f7335d0f39b8
phdfar <iplserver01@gmail.com>
2023-05-20 12:30:45 +0430

    add pth

---
+++ net/simple_binary.py
@@ -0,0 +1,163 @@
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+
+from collections import OrderedDict
+from functools import parfrom backbonecr.fpn import *
+from backbonecr.config import cfg
+import torch
+import osort cfg
+import math
+import os
+import torch
+import torch.nn as nn
+import torch.nn.func
+def aligned_bilinear(tensor, factor):
+    assert tensor.dim() == 4
+    assert factor >= 1
+    assert int(factor) == factor
+
+    if factor == 1:
+        return tensor
+
+    h, w = tensor.size()[2:]
+    tensor = F.pad(tensor, pad=(0, 1, 0, 1), mode='replicate')
+    oh = factor * h + 1
+    ow = factor * w + 1
+    tensor = F.interpolate(tensor,
+                           size=(oh, ow),
+                           mode='bilinear',
+                           align_corners=True)
+    tensor = F.pad(tensor,
+                   pad=(factor // 2, 0, factor // 2, 0),
+                   mode='replicate')
+
+    return tensor[:, :, :oh - 1, :ow - 1]
+tional as F
+
+
+    
+def loadb
+    
+    backbone = build_fcos_resnet_fpn_backbone(cfg)
+    pretrained_wts_file = 'CondInst_MS_R_50_1x.pth'
+    if os.path.exists(pretrained_wts_file):
+        print('Backbone Loaded From ',pretrained_wts_file)
+        restore_dict = torch.load(pretrained_wts_file,map_location=device)
+        restore_dict = {k.replace('backbone.', ''): v for k, v in restore_dict.items()}
+        backbone.load_state_dict(restore_dict, strict=False)
+        _dict(restore_dict, strict=True)
+    return backbone
+
+
+# Define the full model
+class SegmentationModel(nn.Module):
+    def __init__(self, backbone,
+        
+        self.conv_zero = nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, padding='same') seg_head):
+        supebackboneit__()
+        self.backbone = backbone
+        self.seg_heolate(x[1], size=(96, 160), mode='r
+        
+        for i, f in enumerate(features):
+            if i == 0:
+                x = conv_zero(features[f])
+            else:
+                x_p = conv_zero(features[f])
+                target_h, target_w = x.size()[2:]
+                h, w = x_p.size()[2:]
+                assert target_h % h == 0
+                assert target_w % w == 0
+                factor_h, factor_w = target_h // h, target_w // w
+                assert factor_h == factor_w
+                x_p = aligned_bilinear(x_p, factor_h)
+                x = x + x_p
+x[0])
+        z = (features[0]*wx+features[0]
+        seg_map = self.seg_head(z)
+        return seg_map
+
+def run(args,dataloader,dataloader_val):
+        
+    if args.gpu==True:
+        device = torch.device("cuda:0")
+    else:
+        device = torch.device("cpu")
+
+    # Define the segmentation head
+    num_classes = 2 # number of classes in your segmentation task
+    if args.type_output=='diff':
+  
+        nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),      num_classes = 4
+        
+    seg_head = nn.Sequential(
+        nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3,padding='same'),
+        nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
+        nn.Conv2d(in_channels=128, out_channels=num_classes, kernel_size=3,padding='same'),
+        nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
+    )
+
+    backbone = loadbackbone(device)
+    model = SegmentationModel(backbone, seg_head).to(device)
+
+    # Define loss function and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+
+    num_epochs=args.epoch;
+    
+    best_val_loss=10000;
+    # Train the model
+    import sys
+    for eporange(num_epochs):
+        pbar = tqdm(dataloader);i=0;
+        for images, eak, masks in pbaptimizer.zero_grad()
+            outputs = model([images.to(device),weak.to(device)])
+            masks = masks.long().to(device)
+            loss = criterion(outputs, masks)
+            loss.backward()
+            optimizer.step()
+
+            """
+            with torch.no_grad():
+              outputs = model([images.to(device),weak.to(device)])
+              y = outputs.detach().cpu().numpy();g = np.argmax(y[0], axis=0)
+              str_num = '{:05d}'.format(epoch)
+              cv2.imwrite('/content/out/'+str(str_num)+'.png',addtext(g,'epoch-'+str(epoch)))
+            # Update the progress bar with the current loss value
+            """
+            pbar.set_description(f"Epoch {epoch+1}/{num_epochs}, Loss: {loss.item():.4f}")
+            
+            if (i+1)%args.saveiter==0:
+
+              #validation ##################################
+              print('validation...>')
+
+              # Evaluate the model on validation set
+              model.eval()
+              val_loss = 0
+           rch.no_grad():
+                  #pbar_val = tqdm(dataloader_val)
+                 for imagks_val in dataloader_val:
+                      outputs_val = model([images_val.to(device),weak_val.to(device)])
+                      masks_val = masks_val.long().to(device)
+                      tmp = criterion(outputs_val, masks_val).item()
+                      val_loss += tmp
+                      #sys.stdout.write("\033[K") # Clear to the end of line
+                      #pbar_val.set_description('iter: '+str(i)+f" Epoch {epoch+1}/{num_epochs}, VAL-Loss: {tmp:.4f}")
+                      #print('iter: '+str(i)+f" Epoch {epoch+1}/{num_epochs}, VAL-Loss: {tmp:.4f}")
+                      #print("\033[K")
+                      #sys.stdout.write('\r')
+
+              val_loss /= len(dataloader_val)
+              print('iter: '+str(i)+ f" ======>>> Epoch {epoch+1}/{num_epochs}, Mean VAL-Loss: {val_loss:.4f}")
+              # Save the best model based on validation loss
+              #print('')
+              if val_loss < best_val_loss:
+                  best_val_loss = val_loss
+                  #os.system('rm '+args.model_dir)
+                  torch.save(model.state_dict(), args.model_dir)
+
+              model.train()
+              #print('##################################################')
+            i=i+1;
--- run_train.py
+++ run_train.py
@@ -1,12 +1,17 @@
 import random
-from dataloader import correct
+from dataloader import correc,binaryt
 from net import simple_backbone
 
 def start(args):
     if args.task=='correct':
         dataloader_train,dataloader_val=correct.run(args)
         if args.network=='simple_backbone':
-            simple_backbone.run(args,dataloader_train,dataloader_val)
+            simple_backbone.run(args,dataloader_train,dataloa
+    
+    elif args.task=='binary':
+        dataloader_train,dataloader_val=binary.run(args)
+        if args.network=='simple_binary':
+            simple_binary.run(args,dataloader_train,dataloader_val)der_val)
         
         
 
